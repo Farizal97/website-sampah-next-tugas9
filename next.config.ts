7871ee011231f70042721f9372e2191b654f9a1c
Farizal hendra Putra Kusuma <43551882+Farizal97@users.noreply.github.com>
2026-05-17 19:46:19 +0700

    final

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'sb_publishable_0pOC-xQdHvpiwmc2tbykGg_8d--0TE2',
+  },
 };
 
 export default nextConfig;
